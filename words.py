56df9d012125fedea392e7deeeb970813acebc47
Patryk Dziki <patdziki92@gmail.com>
2020-08-23 18:43:40 +0200

    file with sample words

---
+++ words.py
@@ -0,0 +1,10 @@
+class Words:
+    def __init__(self):
+        self.words_list = [
+                        ('addend', 'składnik sumy'),
+                        ('addition', 'dodawanie'),
+                        ('commutative', 'przemienny'), 
+                        ('denominator', 'mianownik ułamka'),
+                        ('difference', 'różnica'),
+                            ]
+
